8c38f3a53fdb8ba43e0344c4efd2cc6a4ac89a12
Frederick Moir <freddiemoir@icloud.com>
2026-09-08 16:15:59 +0100

    Adding user to the list

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@ import React, { useState } from "react";
 
 function App() {
   const [customers, setCustomers] = useState(["Jack", "John", "Harry"]);
+  const [currentUsername, setCurrentUsername] = useState("");
+
+  function changeUsername(e) {
+    setCurrentUsername(e.target.value);
+  }
+
+  function createUser() {
+    setCustomers((customers) => [...customers, currentUsername]);
+  }
 
   return (
     <>
       <h1>Customer Dashboard</h1>
+      <input
+        type="text"
+        placeholder="Username..."
+        value={currentUsername}
+        onChange={changeUsername}
+      />
+      <button onClick={createUser}>Create Customer</button>
       {customers.map((customer, index) => (
-        <p>{customer}</p>
+        <p key={index}>{customer}</p>
       ))}
-
-      <p>Hello freddie</p>
     </>
   );
 }
