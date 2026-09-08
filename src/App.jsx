c47a8933145c8c0213a34c84b24f595ae819b741
Frederick Moir <freddiemoir@icloud.com>
2026-09-08 15:06:01 +0100

    Added title and customers second time

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import React, { useState } from "react";
+
 function App() {
-  return <></>;
+  const [customers, setCustomers] = useState(["Jack", "John", "Harry"]);
+
+  return (
+    <>
+      <h1>Customer Dashboard</h1>
+      {customers.map((customer, index) => (
+        <p>{customer}</p>
+      ))}
+    </>
+  );
 }
 
 export default App;
